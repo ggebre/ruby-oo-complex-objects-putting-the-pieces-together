74b190b3aa74e0aedd6b87a73752ba417bd34264
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-12 23:42:47 -0500

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize (brand)
+        @brand = brand
+        @color = "green"
+        @size = 42
+        @material = "Leather"
+        @condition = "old"
+    end
+
+    def cobble 
+        @condition= "new"
+        puts "Your shoe is as good as new!"
+    end
+end
